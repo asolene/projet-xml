f5375a30cfaac70756fc96c00007f417bf760d72
asolene <solene.allais97@gmail.com>
2021-09-27 13:43:17 +0200

    flux rangés dans un dossier xml sous assets

--- views/pages.php
+++ views/pages.php
@@ -1,13 +1,67 @@
 <?php
 
-$actuXml=simplexml_load_file("actu.xml") or die("Error: Cannot create object");
+$actuXml = simplexml_load_file("../assets/xml/actu.xml");
+$footballXml = simplexml_load_file("../assets/xml/football.xml");
+$basketXml = simplexml_load_file("../assets/xml/basket.xml");
+$tennisXml = simplexml_load_file("../assets/xml/tennis.xml");
+$rugbyXml = simplexml_load_file("../assets/xml/rugby.xml");
 
-$footballXml=simplexml_load_file("football.xml") or die("Error: Cannot create object");
 
-$basketXml=simplexml_load_file("basket.xml") or die("Error: Cannot create object");
+echo $actuXml->channel[0]->title;
 
-$tennisXml=simplexml_load_file("tennis.xml") or die("Error: Cannot create object");
+/// ?????????????????????????????????????????????????????????
 
-$rugbyXml=simplexml_load_file("rugby.xml") or die("Error: Cannot create object");
+    // if (file_exists('actu.xml')) {
+    //     $actuXml = simplexml_load_file('actu.xml');
+
+    //     print_r($actuXml);
+    // } else {
+    //     exit('Echec lors de l\'ouverture du fichier actu.xml.');
+    // }
+
+    // if (file_exists('football.xml')) {
+    //     $footballXml = simplexml_load_file('football.xml');
+
+    //     print_r($footballXml);
+    // } else {
+    //     exit('Echec lors de l\'ouverture du fichier football.xml.');
+    // }
+
+    // if (file_exists('basket.xml')) {
+    //     $basketXml = simplexml_load_file('basket.xml');
+
+    //     print_r($basketXml);
+    // } else {
+    //     exit('Echec lors de l\'ouverture du fichier basket.xml.');
+    // }
+    
+    // if (file_exists('tennis.xml')) {
+    //     $tennisXml = simplexml_load_file('tennis.xml');
+
+    //     print_r($tennisXml);
+    // } else {
+    //     exit('Echec lors de l\'ouverture du fichier tennis.xml.');
+    // }    
+
+    // if (file_exists('rugby.xml')) {
+    //     $rugbyXml = simplexml_load_file('rugby.xml');
+
+    //     print_r($rugbyXml);
+    // } else {
+    //     exit('Echec lors de l\'ouverture du fichier rugby.xml.');
+    // }       
+
+/// ?????????????????????????????????????????????????????????
+
+// noeuds des flux >>>
+
+// title
+// link
+// description
+// lastBuildDate 
+// docs
+// generator
+// language
+
 
 ?>
