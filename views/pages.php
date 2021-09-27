095cf08678fc3a26aeb1a172e9c01f40955c687f
asolene <solene.allais97@gmail.com>
2021-09-27 15:13:58 +0200

    sauvegarde

--- views/pages.php
+++ views/pages.php
@@ -7,54 +7,37 @@ $tennisXml = simplexml_load_file("../assets/xml/tennis.xml");
 $rugbyXml = simplexml_load_file("../assets/xml/rugby.xml");
 
 
-echo $actuXml->channel[0]->title;
+// echo $actuXml->channel[0]->title . '<br>';
+// echo $actuXml->channel[0]->link . '<br>';
+// echo $actuXml->channel[0]->description . '<br>';
+// echo $actuXml->channel[0]->lastBuildDate . '<br>';
+// echo $actuXml->channel[0]->docs . '<br>';
+// echo $actuXml->channel[0]->generator . '<br>';
+// echo $actuXml->channel[0]->language;
 
-/// ?????????????????????????????????????????????????????????
 
-    // if (file_exists('actu.xml')) {
-    //     $actuXml = simplexml_load_file('actu.xml');
+foreach($actuXml->children() as $actu) {
+    echo $actu->title . "<br>" .
+         $actu->link . "<br>" .
+         $actu->description . "<br>" .
+         $actu->lastBuildDate . "<br>" .
+         $actu->docs . "<br>" .
+         $actu->generator . "<br>" .
+         $actu->language . "<br>";
+  }
 
-    //     print_r($actuXml);
-    // } else {
-    //     exit('Echec lors de l\'ouverture du fichier actu.xml.');
-    // }
 
-    // if (file_exists('football.xml')) {
-    //     $footballXml = simplexml_load_file('football.xml');
 
-    //     print_r($footballXml);
-    // } else {
-    //     exit('Echec lors de l\'ouverture du fichier football.xml.');
-    // }
 
-    // if (file_exists('basket.xml')) {
-    //     $basketXml = simplexml_load_file('basket.xml');
 
-    //     print_r($basketXml);
-    // } else {
-    //     exit('Echec lors de l\'ouverture du fichier basket.xml.');
-    // }
-    
-    // if (file_exists('tennis.xml')) {
-    //     $tennisXml = simplexml_load_file('tennis.xml');
 
-    //     print_r($tennisXml);
-    // } else {
-    //     exit('Echec lors de l\'ouverture du fichier tennis.xml.');
-    // }    
 
-    // if (file_exists('rugby.xml')) {
-    //     $rugbyXml = simplexml_load_file('rugby.xml');
 
-    //     print_r($rugbyXml);
-    // } else {
-    //     exit('Echec lors de l\'ouverture du fichier rugby.xml.');
-    // }       
 
-/// ?????????????????????????????????????????????????????????
+
+
 
 // noeuds des flux >>>
-
 // title
 // link
 // description
