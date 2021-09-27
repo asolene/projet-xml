359e633ceb7db71120e1ebdbb1eec28befeb8a2c
asolene <solene.allais97@gmail.com>
2021-09-27 16:34:17 +0200

    update

--- controllers/index-controller.php
+++ controllers/index-controller.php
@@ -7,22 +7,22 @@ $tennisXml = simplexml_load_file("../assets/xml/tennis.xml");
 $rugbyXml = simplexml_load_file("../assets/xml/rugby.xml");
 
 
-// echo $actuXml->channel[0]->title . '<br>';
-// echo $actuXml->channel[0]->link . '<br>';
-// echo $actuXml->channel[0]->description . '<br>';
-// echo $actuXml->channel[0]->lastBuildDate . '<br>';
-// echo $actuXml->channel[0]->docs . '<br>';
-// echo $actuXml->channel[0]->generator . '<br>';
-// echo $actuXml->channel[0]->language;
+// accueil du fil ? 
+echo $actuXml->channel->title . "<br>" .
+     $actuXml->channel->link . "<br>" .
+     $actuXml->channel->description . "<br>" .
+     $actuXml->channel->lastBuildDate . "<br>" .
+     $actuXml->channel->docs . "<br>" .
+     $actuXml->channel->generator . "<br> <br>" ;
 
 
+//artiles >
 echo $actuXml->channel->item[0]->title . '<br>'.
      $actuXml->channel->item[0]->link . '<br>'.
      $actuXml->channel->item[0]->pubDate . '<br>'.
      $actuXml->channel->item[0]->description . '<br>'.
      $actuXml->channel->item[0]->enclosure . '<br>' ; 
 
-;
 
 echo $actuXml->channel->item[1]->title . '<br>'.
      $actuXml->channel->item[1]->link . '<br>'.
@@ -30,32 +30,12 @@ echo $actuXml->channel->item[1]->title . '<br>'.
      $actuXml->channel->item[1]->description . '<br>'.
      $actuXml->channel->item[1]->enclosure . '<br>' ; 
 
-;
+
+
 
 
 
 // faire un for > pour chaque tableau, echo ce qu'il y a au dessus 
-
-// foreach($actuXml->children() as $actu) {
-
-//     echo $actu->title . "<br>" .
-//          $actu->link . "<br>" .
-//          $actu->description . "<br>" .
-//          $actu->lastBuildDate . "<br>" .
-//          $actu->docs . "<br>" .
-//          $actu->generator . "<br>" .
-//          $actu->language . "<br>";
-
-//   }
-
-
-
-
-
-
-
-
-
 
 
 // noeuds des flux >>>
@@ -65,7 +45,7 @@ echo $actuXml->channel->item[1]->title . '<br>'.
 // lastBuildDate 
 // docs
 // generator
-// language
+// language osef
 
 ///////////////
 // title
