27bc4a44069868d2dccc5898076cfb8ec561773d
asolene <solene.allais97@gmail.com>
2021-09-27 11:32:04 +0200

    test

---
+++ views/pages.php
@@ -0,0 +1,13 @@
+<?php
+
+$actuXml=simplexml_load_file("actu.xml") or die("Error: Cannot create object");
+
+$footballXml=simplexml_load_file("football.xml") or die("Error: Cannot create object");
+
+$basketXml=simplexml_load_file("basket.xml") or die("Error: Cannot create object");
+
+$tennisXml=simplexml_load_file("tennis.xml") or die("Error: Cannot create object");
+
+$rugbyXml=simplexml_load_file("rugby.xml") or die("Error: Cannot create object");
+
+?>
