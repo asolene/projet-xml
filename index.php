aa3f315f3f42f0c362f4d99c22f649fa636b934f
asolene <solene.allais97@gmail.com>
2021-09-28 09:02:48 +0200

    les boucles des articles

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 
+// "entête" de l'article
+
 echo $actuXml->channel->title . "<br>" .
      $actuXml->channel->link . "<br>" .
      $actuXml->channel->description . "<br>" .
      $actuXml->channel->lastBuildDate . "<br>" .
      $actuXml->channel->docs . "<br>" .
      $actuXml->channel->generator . "<br> <br>" ;
-
 
 echo $footballXml->channel->title . "<br>" .
      $footballXml->channel->link . "<br>" .
@@ -15,8 +16,29 @@ echo $footballXml->channel->title . "<br>" .
      $footballXml->channel->docs . "<br>" .
      $footballXml->channel->generator . "<br> <br>" ;     
 
+echo $basketXml->channel->title . "<br>" .
+     $basketXml->channel->link . "<br>" .
+     $basketXml->channel->description . "<br>" .
+     $basketXml->channel->lastBuildDate . "<br>" .
+     $basketXml->channel->docs . "<br>" .
+     $basketXml->channel->generator . "<br> <br>" ;    
 
-//artiles 
+echo $tennisXml->channel->title . "<br>" .
+     $tennisXml->channel->link . "<br>" .
+     $tennisXml->channel->description . "<br>" .
+     $tennisXml->channel->lastBuildDate . "<br>" .
+     $tennisXml->channel->docs . "<br>" .
+     $tennisXml->channel->generator . "<br> <br>" ;    
+
+echo $rugbyXml->channel->title . "<br>" .
+     $rugbyXml->channel->link . "<br>" .
+     $rugbyXml->channel->description . "<br>" .
+     $rugbyXml->channel->lastBuildDate . "<br>" .
+     $rugbyXml->channel->docs . "<br>" .
+     $rugbyXml->channel->generator . "<br> <br>" ;    
+
+
+//artiles > boucle pour les afficher
 
 foreach ($actuNews as $key => $news) {
     echo 
