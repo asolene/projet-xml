20f97474ea9137a2993d592ff5dd7180574427fd
antoinenrt <nourtier.antoine@laposte.net>
2021-09-28 11:07:04 +0200

    hello

---
+++ controllers/parameters-controller.php
@@ -0,0 +1,3 @@
+<?php
+include('./views/controllers/parameters-controller.php');
+?>
--- views/pages.php
+++ views/pages.php
@@ -7,51 +7,13 @@ $tennisXml = simplexml_load_file("../assets/xml/tennis.xml");
 $rugbyXml = simplexml_load_file("../assets/xml/rugby.xml");
 
 
-echo $actuXml->channel[0]->title;
+echo ($actuXml=>channel=>title);
+$items=$actuXml=>channel=>item;
+foreach ($items as $key => $value) {
+    var_dump($value);
+}
 
-/// ?????????????????????????????????????????????????????????
 
-    // if (file_exists('actu.xml')) {
-    //     $actuXml = simplexml_load_file('actu.xml');
-
-    //     print_r($actuXml);
-    // } else {
-    //     exit('Echec lors de l\'ouverture du fichier actu.xml.');
-    // }
-
-    // if (file_exists('football.xml')) {
-    //     $footballXml = simplexml_load_file('football.xml');
-
-    //     print_r($footballXml);
-    // } else {
-    //     exit('Echec lors de l\'ouverture du fichier football.xml.');
-    // }
-
-    // if (file_exists('basket.xml')) {
-    //     $basketXml = simplexml_load_file('basket.xml');
-
-    //     print_r($basketXml);
-    // } else {
-    //     exit('Echec lors de l\'ouverture du fichier basket.xml.');
-    // }
-    
-    // if (file_exists('tennis.xml')) {
-    //     $tennisXml = simplexml_load_file('tennis.xml');
-
-    //     print_r($tennisXml);
-    // } else {
-    //     exit('Echec lors de l\'ouverture du fichier tennis.xml.');
-    // }    
-
-    // if (file_exists('rugby.xml')) {
-    //     $rugbyXml = simplexml_load_file('rugby.xml');
-
-    //     print_r($rugbyXml);
-    // } else {
-    //     exit('Echec lors de l\'ouverture du fichier rugby.xml.');
-    // }       
-
-/// ?????????????????????????????????????????????????????????
 
 // noeuds des flux >>>
 
