69196d61e06c5050316cbe38122bb98ed1f39114
antoinenrt <nourtier.antoine@laposte.net>
2021-09-29 11:21:40 +0200

    actus

--- index.php
+++ index.php
@@ -1,93 +1,53 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<section class="home">
+    <div class="container">
 
-// "entête" de l'article
-
-echo $actuXml->channel->title . "<br>" .
-     $actuXml->channel->link . "<br>" .
-     $actuXml->channel->description . "<br>" .
-     $actuXml->channel->lastBuildDate . "<br>" .
-     $actuXml->channel->docs . "<br>" .
-     $actuXml->channel->generator . "<br> <br>" ;
-
-echo $footballXml->channel->title . "<br>" .
-     $footballXml->channel->link . "<br>" .
-     $footballXml->channel->description . "<br>" .
-     $footballXml->channel->lastBuildDate . "<br>" .
-     $footballXml->channel->docs . "<br>" .
-     $footballXml->channel->generator . "<br> <br>" ;     
-
-echo $basketXml->channel->title . "<br>" .
-     $basketXml->channel->link . "<br>" .
-     $basketXml->channel->description . "<br>" .
-     $basketXml->channel->lastBuildDate . "<br>" .
-     $basketXml->channel->docs . "<br>" .
-     $basketXml->channel->generator . "<br> <br>" ;    
-
-echo $tennisXml->channel->title . "<br>" .
-     $tennisXml->channel->link . "<br>" .
-     $tennisXml->channel->description . "<br>" .
-     $tennisXml->channel->lastBuildDate . "<br>" .
-     $tennisXml->channel->docs . "<br>" .
-     $tennisXml->channel->generator . "<br> <br>" ;    
-
-echo $rugbyXml->channel->title . "<br>" .
-     $rugbyXml->channel->link . "<br>" .
-     $rugbyXml->channel->description . "<br>" .
-     $rugbyXml->channel->lastBuildDate . "<br>" .
-     $rugbyXml->channel->docs . "<br>" .
-     $rugbyXml->channel->generator . "<br> <br>" ;    
-
-
-//artiles > boucle pour les afficher
-
-foreach ($actuNews as $key => $news) {
-    echo 
-    $news->title . '<br>' . 
-    $news->link . '<br>' .
-    $news->pubDate . '<br>' .
-    $news->description . '<br>' .
-    $news->enclosure . '<br>' 
-    ;
-}
-
-foreach ($footNews as $key => $news) {
-    echo 
-    $news->title . '<br>' . 
-    $news->link . '<br>' .
-    $news->pubDate . '<br>' .
-    $news->description . '<br>' .
-    $news->enclosure . '<br>' 
-    ;
-}
-
-foreach ($basketNews as $key => $news) {
-    echo 
-    $news->title . '<br>' . 
-    $news->link . '<br>' .
-    $news->pubDate . '<br>' .
-    $news->description . '<br>' .
-    $news->enclosure . '<br>' 
-    ;
-}
-
-foreach ($tennisNews as $key => $news) {
-    echo 
-    $news->title . '<br>' . 
-    $news->link . '<br>' .
-    $news->pubDate . '<br>' .
-    $news->description . '<br>' .
-    $news->enclosure . '<br>' 
-    ;
-}
-
-foreach ($rugbyNews as $key => $news) {
-    echo 
-    $news->title . '<br>' . 
-    $news->link . '<br>' .
-    $news->pubDate . '<br>' .
-    $news->description . '<br>' .
-    $news->enclosure . '<br>' 
-    ;
-}
-
-?>
+    <?php 
+            for ($i=0; $i <=5 ; $i++) { 
+                $news = $actuNews[$i];?>
+                <div class="row">
+                    <div class="col-md-8 col-sm-12 col-xs-12">
+                        <div class="line top">
+                            <div class="textnew">L'actualité du sport en continu.</div>
+                        </div>
+                        <div class="row">
+                            <article class="col-md-12 article-list">
+                                <div class="inner">
+                                    <figure>
+                                        <img src="<?=$news->enclosure['url']?>" alt="Photo article">
+                                    </figure>
+                                    <div class="details">
+                                        <div class="detail">
+                                            <div class="time"><?=$news->pubDate?></div>
+                                        </div>
+                                    <h1><?=$news->title?></h1>
+                                        <p>
+                                            <?=strip_tags($news->description)?>
+                                        </p>
+                                        <footer>
+                                            <a class="btn btn-primary more" href="single.html">
+                                                <div>Lire l'article</div>
+                                                <div><i class="ion-ios-arrow-thin-right"></i></div>
+                                            </a>
+                                        </footer>
+                                    </div>
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                </div>
+            <?php }
+    ?>
+    
+    </div>
+</section>
+</body>
+</html>
