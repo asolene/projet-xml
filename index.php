67fea542f5a5acf27b8cbf959466523269b2bd72
antoinenrt <nourtier.antoine@laposte.net>
2021-09-29 13:42:08 +0200

    actu

--- index.php
+++ index.php
@@ -9,15 +9,114 @@
 <body>
 <section class="home">
     <div class="container">
-
     <?php 
-            for ($i=0; $i <=5 ; $i++) { 
+            for ($i=0; $i <=2 ; $i++) { 
                 $news = $actuNews[$i];?>
                 <div class="row">
                     <div class="col-md-8 col-sm-12 col-xs-12">
-                        <div class="line top">
-                            <div class="textnew">L'actualité du sport en continu.</div>
+                        
+                        <div class="row">
+                            <article class="col-md-12 article-list">
+                                <div class="inner">
+                                    <figure>
+                                        <img src="<?=$news->enclosure['url']?>" alt="Photo article">
+                                    </figure>
+                                    <div class="details">
+                                        <div class="detail">
+                                            <div class="time"><?=$news->pubDate?></div>
+                                        </div>
+                                    <h1><?=$news->title?></h1>
+                                        <p>
+                                            <?=strip_tags($news->description)?>
+                                        </p>
+                                        <footer>
+                                            <a class="btn btn-primary more" href="single.html">
+                                                <div>Lire l'article</div>
+                                                <div><i class="ion-ios-arrow-thin-right"></i></div>
+                                            </a>
+                                        </footer>
+                                    </div>
+                                </div>
+                            </article>
                         </div>
+                    </div>
+                </div>
+            <?php }
+    ?>
+
+    <?php
+            for ($i=0; $i <=2 ; $i++) { 
+                $news = $footNews[$i];?>
+                <div class="row">
+                    <div class="col-md-8 col-sm-12 col-xs-12">
+                        <div class="row">
+                            <article class="col-md-12 article-list">
+                                <div class="inner">
+                                    <figure>
+                                        <img src="<?=$news->enclosure['url']?>" alt="Photo article">
+                                    </figure>
+                                    <div class="details">
+                                        <div class="detail">
+                                            <div class="time"><?=$news->pubDate?></div>
+                                        </div>
+                                    <h1><?=$news->title?></h1>
+                                        <p>
+                                            <?=strip_tags($news->description)?>
+                                        </p>
+                                        <footer>
+                                            <a class="btn btn-primary more" href="single.html">
+                                                <div>Lire l'article</div>
+                                                <div><i class="ion-ios-arrow-thin-right"></i></div>
+                                            </a>
+                                        </footer>
+                                    </div>
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                </div>
+            <?php }
+    ?>
+
+    <?php
+            for ($i=0; $i <=2 ; $i++) { 
+                $news = $basketNews[$i];?>
+                <div class="row">
+                    <div class="col-md-8 col-sm-12 col-xs-12">
+                        <div class="row">
+                            <article class="col-md-12 article-list">
+                                <div class="inner">
+                                    <figure>
+                                        <img src="<?=$news->enclosure['url']?>" alt="Photo article">
+                                    </figure>
+                                    <div class="details">
+                                        <div class="detail">
+                                            <div class="time"><?=$news->pubDate?></div>
+                                        </div>
+                                    <h1><?=$news->title?></h1>
+                                        <p>
+                                            <?=strip_tags($news->description)?>
+                                        </p>
+                                        <footer>
+                                            <a class="btn btn-primary more" href="single.html">
+                                                <div>Lire l'article</div>
+                                                <div><i class="ion-ios-arrow-thin-right"></i></div>
+                                            </a>
+                                        </footer>
+                                    </div>
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                </div>
+            <?php }
+    ?>
+
+    <?php
+            for ($i=0; $i <=2 ; $i++) { 
+                $news = $tennisNews[$i];?>
+                <div class="row">
+                    <div class="col-md-8 col-sm-12 col-xs-12">
                         <div class="row">
                             <article class="col-md-12 article-list">
                                 <div class="inner">
@@ -47,6 +146,39 @@
             <?php }
     ?>
     
+    <?php
+            for ($i=0; $i <=2 ; $i++) { 
+                $news = $rugbyNews[$i];?>
+                <div class="row">
+                    <div class="col-md-8 col-sm-12 col-xs-12">
+                        <div class="row">
+                            <article class="col-md-12 article-list">
+                                <div class="inner">
+                                    <figure>
+                                        <img src="<?=$news->enclosure['url']?>" alt="Photo article">
+                                    </figure>
+                                    <div class="details">
+                                        <div class="detail">
+                                            <div class="time"><?=$news->pubDate?></div>
+                                        </div>
+                                    <h1><?=$news->title?></h1>
+                                        <p>
+                                            <?=strip_tags($news->description)?>
+                                        </p>
+                                        <footer>
+                                            <a class="btn btn-primary more" href="single.html">
+                                                <div>Lire l'article</div>
+                                                <div><i class="ion-ios-arrow-thin-right"></i></div>
+                                            </a>
+                                        </footer>
+                                    </div>
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                </div>
+            <?php }
+    ?>
     </div>
 </section>
 </body>
