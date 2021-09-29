83d8b2d2bafa182f1cf36f7ccdc7f10806492ba8
asolene <solene.allais97@gmail.com>
2021-09-29 13:49:37 +0200

    modifications (xml via l'url)

--- controllers/index-controller.php
+++ controllers/index-controller.php
@@ -1,10 +1,24 @@
 <?php
+$urlActu = "https://rmcsport.bfmtv.com/rss/fil-sport/";
+$actuXml = simplexml_load_file($urlActu);
 
-$actuXml = simplexml_load_file("../assets/xml/actu.xml");
-$footballXml = simplexml_load_file("../assets/xml/football.xml");
-$basketXml = simplexml_load_file("../assets/xml/basket.xml");
-$tennisXml = simplexml_load_file("../assets/xml/tennis.xml");
-$rugbyXml = simplexml_load_file("../assets/xml/rugby.xml");
+$urlFoot = "https://rmcsport.bfmtv.com/rss/football/";
+$footballXml = simplexml_load_file($urlFoot);
+
+$urlBasket = "https://rmcsport.bfmtv.com/rss/basket/";
+$basketXml = simplexml_load_file($urlBasket);
+
+$urlTennis = "https://rmcsport.bfmtv.com/rss/tennis/";
+$tennisXml = simplexml_load_file($urlTennis);
+
+$urlRugby = "https://rmcsport.bfmtv.com/rss/rugby/";
+$rugbyXml = simplexml_load_file($urlRugby);
+
+// $actuXml = simplexml_load_file("../assets/xml/actu.xml");
+// $footballXml = simplexml_load_file("../assets/xml/football.xml");
+// $basketXml = simplexml_load_file("../assets/xml/basket.xml");
+// $tennisXml = simplexml_load_file("../assets/xml/tennis.xml");
+// $rugbyXml = simplexml_load_file("../assets/xml/rugby.xml");
 
 
 $actuNews = $actuXml->channel->item;
--- controllers/pages-controller.php
+++ controllers/pages-controller.php
@@ -2,29 +2,35 @@
 
 $topic = filter_input(INPUT_GET,'topic', FILTER_SANITIZE_STRING);
 
+$urlActu = "https://rmcsport.bfmtv.com/rss/fil-sport/";
+$urlFoot = "https://rmcsport.bfmtv.com/rss/football/";
+$urlBasket = "https://rmcsport.bfmtv.com/rss/basket/";
+$urlTennis = "https://rmcsport.bfmtv.com/rss/tennis/";
+$urlRugby = "https://rmcsport.bfmtv.com/rss/rugby/";
+
 switch ($topic) {
     case 'actu':
-        $xml = simplexml_load_file("../assets/xml/actu.xml");
+        $xml = simplexml_load_file($urlActu);
         $feed = $xml->channel->item;
         break;
     
     case 'football':
-        $xml = simplexml_load_file("../assets/xml/football.xml");
+        $xml = simplexml_load_file($urlFoot);
         $feed = $xml->channel->item;
         break;
 
     case 'basket':
-        $xml = simplexml_load_file("../assets/xml/basket.xml");
+        $xml = simplexml_load_file($urlBasket);
         $feed = $xml->channel->item;
         break;
     
     case 'tennis':
-        $xml = simplexml_load_file("../assets/xml/tennis.xml");
+        $xml = simplexml_load_file($urlTennis);
         $feed = $xml->channel->item;
         break;
 
     case 'rugby':
-        $xml = simplexml_load_file("../assets/xml/rugby.xml");
+        $xml = simplexml_load_file($urlRugby);
         $feed = $xml->channel->item;
         break;
 }
